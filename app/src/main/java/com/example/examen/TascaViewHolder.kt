0c0aeb879a901165df6d60b5b46d08ce244d445a
eric <eric.parada.r@gmail.com>
2026-02-03 20:10:08 +0100

    final

--- app/src/main/java/com/example/examen/TascaViewHolder.kt
+++ app/src/main/java/com/example/examen/TascaViewHolder.kt
@@ -1,5 +1,6 @@
 package com.example.examen
 
+import android.graphics.Color
 import android.view.View
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -15,6 +16,15 @@ class TascaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     fun renderitza(tasca: Tasca) {
         tvNom.text = tasca.nom
+        tvCategoria.text = tasca.categoria as CharSequence?
         tvData.text = tasca.data
+        val color = when (tvEstat.text){
+            "No començada" -> "#FF9800"
+            "En curs" -> "#2196F3"
+            "Finalitzada" -> "#4CAF50"
+            else -> {"#000000"}
+        }
+        tvEstat.setTextColor(Color.parseColor(color))
+        tvEstat.text = tasca.estat as CharSequence?
     }
 }
